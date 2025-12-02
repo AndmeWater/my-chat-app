b286d37800c25a1d32205d1bf3c077360434cc1d
Linlu <lupengchong.lpc@alibaba-inc.com>
2025-12-02 16:39:34 +0800

    11

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// 假设你的 github 仓库名是 my-chat-app
-export default defineConfig({
-  plugins: [vue()],
-  base: '/my-chat-app/', 
-})
